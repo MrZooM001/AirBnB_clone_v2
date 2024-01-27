eb260f0fc417e439b5dc458e4c1d9d2bd85edc09
Hazem Magdy <hhazem0010@gmail.com>
2024-01-27 09:56:11 +0200

    update task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""A module that generates a .tgz archive from the contents of a web static
+using Fabric."""
 from fabric.api import local
 from datetime import datetime
 from os import stat
 
 
 def do_pack():
+    """A function to compress a web static files into .tgz archive file."""
     local('mkdir -p versions')
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     tgz_file = "versions/web_static_{}.tgz".format(timestamp)
