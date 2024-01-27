5fe00dacfd3a48678ae7af67c478130081036f1e
Hazem Magdy <hhazem0010@gmail.com>
2024-01-27 12:20:15 +0200

    fix style task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,12 +9,12 @@ def do_deploy(archive_path):
     """A function to distribute an archive to web servers using SSH"""
     if exists(archive_path) is False:
         return False
-    
+
     try:
         file_name = archive_path.split("/")[-1]
         file_without_extention = file_name.split(".")[0]
         file_path = "/data/web_static/releases/"
-        
+
         put(archive_path, '/tmp/')
         run('sudo mkdir -p {}{}/'.format(file_path, file_without_extention))
         run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_name, file_path, file_without_extention))
